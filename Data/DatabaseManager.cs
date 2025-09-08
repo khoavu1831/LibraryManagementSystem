bdb48d77131cd64bfeb51e6cae9d31f284847b57
kaiwu24 <khoavu1831@gmail.com>
2025-09-09 00:48:00 +0700

    chore: test EF

---
+++ Data/DatabaseManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Data
+{
+    public static class DatabaseManager
+    {
+        private static readonly string namedb = new LibraryDbContext().Database.GetDbConnection().Database;
+        public static void CreateDatabase()
+        {
+            var dbcontext = new LibraryDbContext();
+            var rs = dbcontext.Database.EnsureCreated();
+            if (rs) MessageBox.Show($"created database {namedb}");
+        }
+        public static void DeleteDatabase()
+        {
+            var dbcontext = new LibraryDbContext();
+            var rs = dbcontext.Database.EnsureDeleted();
+            if (rs) MessageBox.Show($"droped database {namedb}");
+        }
+    }
+}
---
+++ Data/LibraryDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        private readonly string connectionString = @"
+                                                   server=localhost;
+                                                   port=3306;
+                                                   user=root;
+                                                   password=;
+                                                   database=librarydbtest";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            // Dùng gói Pomelo nên phải thêm tham số Version
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}
