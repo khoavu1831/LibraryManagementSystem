728031a99fd8b477e9a23b54b2360d319a01666e
dbp <dbp0932@gmail.com>
2025-09-30 08:39:37 +0700

    feat(UI_VaiTro): thêm giao diện thêm, sửa và chi tiét

---
+++ Views/UserControls/QLNhanVien/VaiTro/UcVaiTro.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace LibraryManagementSystem.Views.UserControls.QLNhanVien.VaiTro
+{
+    partial class UcVaiTro
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelCRUD = new Panel();
+            panelSearch = new TableLayoutPanel();
+            btnTimKiem = new Button();
+            txtBoxTimKiem = new TextBox();
+            btnLamMoi = new Button();
+            btnChiTiet = new Button();
+            btnXoa = new Button();
+            btnSua = new Button();
+            btnThem = new Button();
+            dataGridView1 = new DataGridView();
+            panelCRUD.SuspendLayout();
+            panelSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panelCRUD
+            // 
+            panelCRUD.BackColor = Color.White;
+            panelCRUD.Controls.Add(panelSearch);
+            panelCRUD.Controls.Add(btnChiTiet);
+            panelCRUD.Controls.Add(btnXoa);
+            panelCRUD.Controls.Add(btnSua);
+            panelCRUD.Controls.Add(btnThem);
+            panelCRUD.Dock = DockStyle.Top;
+            panelCRUD.Location = new Point(0, 0);
+            panelCRUD.Margin = new Padding(3, 4, 3, 4);
+            panelCRUD.Name = "panelCRUD";
+            panelCRUD.Size = new Size(885, 96);
+            panelCRUD.TabIndex = 2;
+            // 
+            // panelSearch
+            // 
+            panelSearch.ColumnCount = 3;
+            panelSearch.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25.8620682F));
+            panelSearch.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 60.3448257F));
+            panelSearch.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 14.08046F));
+            panelSearch.Controls.Add(btnTimKiem, 0, 0);
+            panelSearch.Controls.Add(txtBoxTimKiem, 1, 0);
+            panelSearch.Controls.Add(btnLamMoi, 2, 0);
+            panelSearch.Dock = DockStyle.Right;
+            panelSearch.Location = new Point(487, 0);
+            panelSearch.Margin = new Padding(3, 4, 3, 4);
+            panelSearch.Name = "panelSearch";
+            panelSearch.RowCount = 1;
+            panelSearch.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            panelSearch.Size = new Size(398, 96);
+            panelSearch.TabIndex = 9;
+            // 
+            // btnTimKiem
+            // 
+            btnTimKiem.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnTimKiem.BackColor = Color.White;
+            btnTimKiem.FlatAppearance.BorderColor = Color.Black;
+            btnTimKiem.FlatStyle = FlatStyle.Flat;
+            btnTimKiem.Location = new Point(3, 32);
+            btnTimKiem.Margin = new Padding(3, 4, 3, 4);
+            btnTimKiem.Name = "btnTimKiem";
+            btnTimKiem.Size = new Size(96, 32);
+            btnTimKiem.TabIndex = 8;
+            btnTimKiem.Text = "Tìm Kiếm";
+            btnTimKiem.UseVisualStyleBackColor = false;
+            // 
+            // txtBoxTimKiem
+            // 
+            txtBoxTimKiem.Anchor = AnchorStyles.None;
+            txtBoxTimKiem.Location = new Point(106, 34);
+            txtBoxTimKiem.Margin = new Padding(3, 4, 3, 4);
+            txtBoxTimKiem.Name = "txtBoxTimKiem";
+            txtBoxTimKiem.PlaceholderText = "Tìm kiếm...";
+            txtBoxTimKiem.Size = new Size(231, 27);
+            txtBoxTimKiem.TabIndex = 9;
+            // 
+            // btnLamMoi
+            // 
+            btnLamMoi.BackColor = Color.White;
+            btnLamMoi.Dock = DockStyle.Fill;
+            btnLamMoi.FlatAppearance.BorderSize = 0;
+            btnLamMoi.FlatStyle = FlatStyle.Flat;
+            btnLamMoi.Image = Properties.Resources.iconRefesh;
+            btnLamMoi.Location = new Point(341, 0);
+            btnLamMoi.Margin = new Padding(0);
+            btnLamMoi.Name = "btnLamMoi";
+            btnLamMoi.Size = new Size(57, 96);
+            btnLamMoi.TabIndex = 7;
+            btnLamMoi.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnLamMoi.UseVisualStyleBackColor = false;
+            // 
+            // btnChiTiet
+            // 
+            btnChiTiet.BackColor = Color.White;
+            btnChiTiet.Dock = DockStyle.Left;
+            btnChiTiet.FlatAppearance.BorderSize = 0;
+            btnChiTiet.FlatStyle = FlatStyle.Flat;
+            btnChiTiet.Image = Properties.Resources.iconCT;
+            btnChiTiet.Location = new Point(264, 0);
+            btnChiTiet.Margin = new Padding(0);
+            btnChiTiet.Name = "btnChiTiet";
+            btnChiTiet.Size = new Size(88, 96);
+            btnChiTiet.TabIndex = 3;
+            btnChiTiet.Text = "Chi Tiết";
+            btnChiTiet.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnChiTiet.UseVisualStyleBackColor = false;
+            // 
+            // btnXoa
+            // 
+            btnXoa.BackColor = Color.White;
+            btnXoa.Dock = DockStyle.Left;
+            btnXoa.FlatAppearance.BorderSize = 0;
+            btnXoa.FlatStyle = FlatStyle.Flat;
+            btnXoa.Image = Properties.Resources.iconxoa;
+            btnXoa.Location = new Point(176, 0);
+            btnXoa.Margin = new Padding(0);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(88, 96);
+            btnXoa.TabIndex = 2;
+            btnXoa.Text = "Xóa";
+            btnXoa.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnXoa.UseVisualStyleBackColor = false;
+            // 
+            // btnSua
+            // 
+            btnSua.BackColor = Color.White;
+            btnSua.Dock = DockStyle.Left;
+            btnSua.FlatAppearance.BorderSize = 0;
+            btnSua.FlatStyle = FlatStyle.Flat;
+            btnSua.Image = Properties.Resources.iconsua;
+            btnSua.Location = new Point(88, 0);
+            btnSua.Margin = new Padding(0);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(88, 96);
+            btnSua.TabIndex = 1;
+            btnSua.Text = "Sửa";
+            btnSua.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnSua.UseVisualStyleBackColor = false;
+            // 
+            // btnThem
+            // 
+            btnThem.BackColor = Color.White;
+            btnThem.Dock = DockStyle.Left;
+            btnThem.FlatAppearance.BorderSize = 0;
+            btnThem.FlatStyle = FlatStyle.Flat;
+            btnThem.Image = Properties.Resources.iconthem;
+            btnThem.Location = new Point(0, 0);
+            btnThem.Margin = new Padding(0);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(88, 96);
+            btnThem.TabIndex = 0;
+            btnThem.Text = "Thêm";
+            btnThem.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnThem.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 96);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(885, 655);
+            dataGridView1.TabIndex = 3;
+            // 
+            // UcVaiTro
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(dataGridView1);
+            Controls.Add(panelCRUD);
+            Name = "UcVaiTro";
+            Size = new Size(885, 751);
+            panelCRUD.ResumeLayout(false);
+            panelSearch.ResumeLayout(false);
+            panelSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelCRUD;
+        private TableLayoutPanel panelSearch;
+        private Button btnTimKiem;
+        private TextBox txtBoxTimKiem;
+        private Button btnLamMoi;
+        private Button btnChiTiet;
+        private Button btnXoa;
+        private Button btnSua;
+        private Button btnThem;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ Views/UserControls/QLNhanVien/VaiTro/formThemVaiTro.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace LibraryManagementSystem.Views.UserControls.QLNhanVien.VaiTro
+{
+    partial class formThemVaiTro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            panel2 = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            textBoxHVT = new TextBox();
+            label2 = new Label();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            btnThem = new Button();
+            btnHuy = new Button();
+            panel2.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.HotTrack;
+            label1.Dock = DockStyle.Top;
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(521, 125);
+            label1.TabIndex = 3;
+            label1.Text = "THÊM VAI TRÒ";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(tableLayoutPanel2);
+            panel2.Controls.Add(tableLayoutPanel1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 125);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(521, 424);
+            panel2.TabIndex = 23;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30.2521F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 69.7479F));
+            tableLayoutPanel1.Controls.Add(textBoxHVT, 1, 0);
+            tableLayoutPanel1.Controls.Add(label2, 0, 0);
+            tableLayoutPanel1.Location = new Point(72, 67);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(377, 42);
+            tableLayoutPanel1.TabIndex = 17;
+            // 
+            // textBoxHVT
+            // 
+            textBoxHVT.Dock = DockStyle.Fill;
+            textBoxHVT.Location = new Point(117, 3);
+            textBoxHVT.Multiline = true;
+            textBoxHVT.Name = "textBoxHVT";
+            textBoxHVT.Size = new Size(257, 36);
+            textBoxHVT.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 42);
+            label2.TabIndex = 1;
+            label2.Text = "Tên vai trò:";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(btnThem, 0, 0);
+            tableLayoutPanel2.Controls.Add(btnHuy, 1, 0);
+            tableLayoutPanel2.Location = new Point(75, 155);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel2.Size = new Size(371, 54);
+            tableLayoutPanel2.TabIndex = 20;
+            // 
+            // btnThem
+            // 
+            btnThem.Dock = DockStyle.Top;
+            btnThem.Location = new Point(3, 3);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(179, 46);
+            btnThem.TabIndex = 15;
+            btnThem.Text = "Thêm";
+            btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnHuy
+            // 
+            btnHuy.Dock = DockStyle.Top;
+            btnHuy.Location = new Point(188, 3);
+            btnHuy.Name = "btnHuy";
+            btnHuy.Size = new Size(180, 46);
+            btnHuy.TabIndex = 18;
+            btnHuy.Text = "Hủy";
+            btnHuy.UseVisualStyleBackColor = true;
+            // 
+            // formThemVaiTro
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(521, 549);
+            Controls.Add(panel2);
+            Controls.Add(label1);
+            Name = "formThemVaiTro";
+            Text = "formThemVaiTro";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel2;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox textBoxHVT;
+        private Label label2;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button btnThem;
+        private Button btnHuy;
+    }
+}
