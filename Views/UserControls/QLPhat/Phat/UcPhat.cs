c9c8129dae8bd639417556f62a706cc7e09eda4c
LuuD04N <doanphongluu082@gmail.com>
2025-09-30 10:05:38 +0700

    Feat: Penalty UC

---
+++ Views/UserControls/QLPhat/Phat/UcPhat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagementSystem.Views.UserControls.QLPhat.Phat
+{
+    public partial class UcPhat : UserControl
+    {
+        public UcPhat()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
