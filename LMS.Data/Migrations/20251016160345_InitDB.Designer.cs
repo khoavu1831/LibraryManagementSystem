c677414446ec4b33a6545d0492e6f0857674d694
HoangViet158 <kirito15082004@gmail.com>
2025-11-12 21:12:22 +0700

    update theo main moi nhat

--- LMS.Data/Migrations/20251016160345_InitDB.Designer.cs
+++ LMS.Data/Migrations/20251016160345_InitDB.Designer.cs
@@ -16,7 +16,7 @@ namespace LMS.Migrations
     partial class InitDB
     {
         /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
