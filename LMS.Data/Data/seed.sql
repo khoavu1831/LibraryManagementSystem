42853e642d3bd4ba52dae18fc62c1e1b41960744
kaiwu24 <khoavu1831@gmail.com>
2025-12-02 13:43:37 +0700

    ft: thêm migrations và file seed dể không phải CREATE TABLE bằng file!

--- LMS.Data/Data/seed.sql
+++ LMS.Data/Data/seed.sql
@@ -1,69 +1,470 @@
--- Thêm NXB
-INSERT INTO NXB (TenNXB, DiaChi, SDT) VALUES
-('NXB Kim Đồng', 'Hà Nội', '0123456789'),
-('NXB Trẻ', 'TP. HCM', '0987654321');
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.4.3 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.8.0.6908
+-- --------------------------------------------------------
 
--- Thêm Thể loại
-INSERT INTO TheLoai (TenTheLoai) VALUES
-('Văn học'),
-('Khoa học'),
-('Thiếu nhi');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Thêm Tác giả
-INSERT INTO TacGia (TenTacGia, NgaySinh, NoiSinh, SDT) VALUES
-('Nguyễn Nhật Ánh', '1955-05-07', 'Quảng Nam', '0901234567'),
-('J.K. Rowling', '1965-07-31', 'Yate, Anh', '0912345678');
+-- Dumping data for table library.bansaosach: ~6 rows (approximately)
+INSERT INTO `bansaosach` (`IdBanSaoSach`, `IdSach`, `TinhTrangSach`) VALUES
+	('S1_1', 1, 'Tot'),
+	('S1_10', 1, 'Tot'),
+	('S1_11', 1, 'Tot'),
+	('S1_12', 1, 'Tot'),
+	('S1_13', 1, 'Tot'),
+	('S1_14', 1, 'Tot'),
+	('S1_15', 1, 'Tot'),
+	('S1_16', 1, 'Tot'),
+	('S1_17', 1, 'Tot'),
+	('S1_18', 1, 'Tot'),
+	('S1_19', 1, 'Tot'),
+	('S1_2', 1, 'Tot'),
+	('S1_20', 1, 'Tot'),
+	('S1_21', 1, 'Tot'),
+	('S1_22', 1, 'Tot'),
+	('S1_23', 1, 'Tot'),
+	('S1_24', 1, 'Tot'),
+	('S1_25', 1, 'Tot'),
+	('S1_26', 1, 'Tot'),
+	('S1_27', 1, 'Tot'),
+	('S1_28', 1, 'Tot'),
+	('S1_29', 1, 'Tot'),
+	('S1_3', 1, 'Tot'),
+	('S1_30', 1, 'Tot'),
+	('S1_31', 1, 'Tot'),
+	('S1_32', 1, 'Tot'),
+	('S1_33', 1, 'Tot'),
+	('S1_34', 1, 'Tot'),
+	('S1_35', 1, 'Tot'),
+	('S1_36', 1, 'Tot'),
+	('S1_37', 1, 'Tot'),
+	('S1_38', 1, 'Tot'),
+	('S1_39', 1, 'Tot'),
+	('S1_4', 1, 'Tot'),
+	('S1_40', 1, 'Tot'),
+	('S1_41', 1, 'Tot'),
+	('S1_42', 1, 'Tot'),
+	('S1_43', 1, 'Tot'),
+	('S1_44', 1, 'Tot'),
+	('S1_45', 1, 'Tot'),
+	('S1_46', 1, 'Tot'),
+	('S1_47', 1, 'Tot'),
+	('S1_48', 1, 'Tot'),
+	('S1_49', 1, 'Tot'),
+	('S1_5', 1, 'Tot'),
+	('S1_50', 1, 'Tot'),
+	('S1_51', 1, 'Tot'),
+	('S1_52', 1, 'Tot'),
+	('S1_53', 1, 'Tot'),
+	('S1_54', 1, 'Tot'),
+	('S1_55', 1, 'Tot'),
+	('S1_56', 1, 'Tot'),
+	('S1_57', 1, 'Tot'),
+	('S1_6', 1, 'Tot'),
+	('S1_7', 1, 'Tot'),
+	('S1_8', 1, 'Tot'),
+	('S1_9', 1, 'Tot'),
+	('S2_1', 2, 'Tot'),
+	('S2_10', 2, 'Tot'),
+	('S2_11', 2, 'Tot'),
+	('S2_12', 2, 'Tot'),
+	('S2_13', 2, 'Tot'),
+	('S2_14', 2, 'Tot'),
+	('S2_15', 2, 'Tot'),
+	('S2_16', 2, 'Tot'),
+	('S2_17', 2, 'Tot'),
+	('S2_18', 2, 'Tot'),
+	('S2_19', 2, 'Tot'),
+	('S2_2', 2, 'ChoMuon'),
+	('S2_20', 2, 'Tot'),
+	('S2_21', 2, 'Tot'),
+	('S2_22', 2, 'Tot'),
+	('S2_23', 2, 'Tot'),
+	('S2_24', 2, 'Tot'),
+	('S2_25', 2, 'Tot'),
+	('S2_26', 2, 'Tot'),
+	('S2_27', 2, 'Tot'),
+	('S2_28', 2, 'Tot'),
+	('S2_29', 2, 'Tot'),
+	('S2_3', 2, 'Tot'),
+	('S2_30', 2, 'Tot'),
+	('S2_31', 2, 'Tot'),
+	('S2_32', 2, 'Tot'),
+	('S2_33', 2, 'Tot'),
+	('S2_34', 2, 'Tot'),
+	('S2_35', 2, 'Tot'),
+	('S2_36', 2, 'Tot'),
+	('S2_37', 2, 'Tot'),
+	('S2_38', 2, 'Tot'),
+	('S2_4', 2, 'Tot'),
+	('S2_5', 2, 'Tot'),
+	('S2_6', 2, 'Tot'),
+	('S2_7', 2, 'Tot'),
+	('S2_8', 2, 'Tot'),
+	('S2_9', 2, 'Tot'),
+	('S3_1', 3, 'Tot'),
+	('S3_10', 3, 'Tot'),
+	('S3_11', 3, 'Tot'),
+	('S3_12', 3, 'Tot'),
+	('S3_13', 3, 'Tot'),
+	('S3_14', 3, 'Tot'),
+	('S3_15', 3, 'Tot'),
+	('S3_16', 3, 'Tot'),
+	('S3_17', 3, 'Tot'),
+	('S3_18', 3, 'Tot'),
+	('S3_19', 3, 'Tot'),
+	('S3_2', 3, 'Tot'),
+	('S3_20', 3, 'Tot'),
+	('S3_21', 3, 'Tot'),
+	('S3_22', 3, 'Tot'),
+	('S3_3', 3, 'Tot'),
+	('S3_4', 3, 'Tot'),
+	('S3_5', 3, 'Tot'),
+	('S3_6', 3, 'Tot'),
+	('S3_7', 3, 'Tot'),
+	('S3_8', 3, 'Tot'),
+	('S3_9', 3, 'Tot');
 
--- Thêm Sách
-INSERT INTO Sach (IdSach, IdNXB, NamXuatBan, TenSach, MoTa, SoTrang, SoLuongBanSao, GiaTien) VALUES
-('1', 1, 2010, 'Kính vạn hoa', 'Truyện dài nhiều tập', 200, 5, 14000),
-('2', 2, 1997, 'Harry Potter and the Philosopher''s Stone', 'Tập 1 Harry Potter', 350, 10, 20000);
+-- Dumping data for table library.chitietphieumuon: ~6 rows (approximately)
+INSERT INTO `chitietphieumuon` (`IdChiTietPhieuMuon`, `IdPhieuMuon`, `IdBanSaoSach`, `NgayTra`, `TinhTrangTra`) VALUES
+	(1, 1, 'S1_1', '2025-11-24 13:59:46.918376', 'Tot'),
+	(2, 2, 'S1_1', '2025-11-24 16:06:39.525140', 'Tot'),
+	(3, 2, 'S1_10', '2025-11-24 16:06:39.609429', 'Tot'),
+	(4, 2, 'S1_11', '2025-11-24 16:06:39.638465', 'Tot'),
+	(5, 3, 'S1_12', '2025-11-24 16:03:00.705763', 'Tot'),
+	(6, 3, 'S1_13', '2025-11-24 16:03:44.464862', 'Tot');
 
--- Thêm dữ liệu Nhà cung cấp
-INSERT INTO NCC (TenNCC, DiaChi, SDT) VALUES
-('Công ty Phát Hành Sách Hà Nội', 'Hà Nội', '0901112233'),
-('Công ty Sách Minh Long', 'TP. HCM', '0902223344'),
-('Nhà Sách Fahasa', 'Đà Nẵng', '0903334455'),
-('Công ty TNHH Sách Alpha', 'Hà Nội', '0904445566'),
-('Công ty Sách và Thiết bị Trường học TP.HCM', 'TP. HCM', '0905556677');
+-- Dumping data for table library.chitietphieunhap: ~17 rows (approximately)
+INSERT INTO `chitietphieunhap` (`IdChiTietPhieuNhap`, `IdPhieuNhap`, `IdSach`, `SoLuong`, `GiaTien`) VALUES
+	(1, 1, 1, 10, 14000.00),
+	(2, 2, 2, 20, 20000.00),
+	(3, 3, 1, 4, 14000.00),
+	(4, 4, 1, 3, 14000.00),
+	(5, 4, 2, 2, 20000.00),
+	(6, 4, 3, 1, 100000.00),
+	(7, 5, 1, 1, 14000.00),
+	(8, 6, 2, 2, 20000.00),
+	(9, 6, 3, 3, 100000.00),
+	(10, 7, 3, 6, 100000.00),
+	(11, 8, 2, 6, 20000.00),
+	(12, 8, 3, 6, 100000.00),
+	(13, 9, 1, 3, 14000.00),
+	(14, 9, 2, 6, 20000.00),
+	(15, 9, 3, 6, 100000.00),
+	(16, 10, 1, 10, 14000.00),
+	(17, 11, 1, 12, 14000.00),
+	(18, 12, 1, 12, 14000.00);
 
--- Mapping sách - thể loại
-INSERT INTO Sach_TheLoai (IdSach, IdTheLoai) VALUES
-('1', 1),
-('2', 2);
+-- Dumping data for table library.chitietphieuphat: ~4 rows (approximately)
+INSERT INTO `chitietphieuphat` (`IdChiTietPhieuPhat`, `IdPhieuPhat`, `IdChiTietPhieuMuon`, `IdMucPhat`, `SoNgayTreHen`, `TienPhatTra`) VALUES
+	(1, 1, 1, 1, 48, 240000.00),
+	(2, 2, 2, 1, 16, 80000.00),
+	(3, 2, 3, 1, 16, 80000.00),
+	(4, 2, 4, 1, 16, 80000.00);
 
--- Mapping sách - tác giả
-INSERT INTO Sach_TacGia (IdSach, IdTacGia) VALUES
-('1', 1),
-('2', 2);
+-- Dumping data for table library.docgia: ~2 rows (approximately)
+INSERT INTO `docgia` (`IdDocGia`, `TenDocGia`, `DiaChi`, `NgaySinh`, `SDT`, `Email`) VALUES
+	(1, 'Trần Văn A', 'Hà Nội', '2000-01-01 00:00:00.000000', '0900000001', 'a@gmail.com'),
+	(2, 'Nguyễn Thị B', 'Đà Nẵng', '1999-02-02 00:00:00.000000', '0900000002', 'b@gmail.com');
 
--- Thêm Bản sao sách (IdBanSaoSach là string)
-INSERT INTO BanSaoSach (IdBanSaoSach, IdSach, TinhTrangSach) VALUES
-('S1_1', '1', 'Tot'),
-('S1_2', '1', 'Tot'),
-('S2_1', '2', 'Tot'),
-('S2_2', '2', 'ChoMuon');
+-- Dumping data for table library.mucphat: ~0 rows (approximately)
+INSERT INTO `mucphat` (`IdMucPhat`, `TenMucPhat`, `SoTienPhat`, `MoTa`, `LoaiPhat`, `IsActive`) VALUES
+	(1, 'Phạt trễ hạn', 5000.00, 'Phạt mỗi ngày trễ', 'Perday', 1);
 
--- Thêm độc giả
-INSERT INTO DocGia (TenDocGia, DiaChi, NgaySinh, SDT, Email) VALUES
-('Trần Văn A', 'Hà Nội', '2000-01-01', '0900000001', 'a@gmail.com'),
-('Nguyễn Thị B', 'Đà Nẵng', '1999-02-02', '0900000002', 'b@gmail.com');
+-- Dumping data for table library.ncc: ~4 rows (approximately)
+INSERT INTO `ncc` (`IdNCC`, `TenNCC`, `DiaChi`, `SDT`) VALUES
+	(1, 'Công ty Phát Hành Sách Hà Nội', 'Hà Nội', '0901112233'),
+	(2, 'Công ty Sách Minh Long', 'TP. HCM', '0902223344'),
+	(3, 'Nhà Sách Fahasa', 'Đà Nẵng', '0903334455'),
+	(4, 'Công ty TNHH Sách Alpha', 'Hà Nội', '0904445566');
 
--- Thêm thẻ thành viên
-INSERT INTO TheThanhVien (IdDocGia, NgayCap, NgayHetHan, TrangThai) VALUES
-(1, '2025-01-01', '2026-01-01', 'HoatDong'),
-(2, '2025-01-01', '2026-01-01', 'HoatDong');
+-- Dumping data for table library.nhanvien: ~3 rows (approximately)
+INSERT INTO `nhanvien` (`IdNhanVien`, `IdTaiKhoan`, `TenNhanVien`, `NgaySinh`, `DiaChi`, `SDT`, `Email`, `GioiTinh`) VALUES
+	(1, 1, 'Nguyễn Văn D', '1990-03-03 00:00:00.000000', 'Hà Nội', '0900000003', 'c@gmail.com', 'Nam'),
+	(7, 10, 'dinhbaphong', '2025-11-03 10:28:03.404875', 'dongnai', '0932788999', 'zxczxc@gmail.com', 'Nam'),
+	(8, 11, 'Đoàn Phong Lưu', '2004-03-22 20:23:11.955000', 'Đồng Nai', '0395455082', 'ehehe@edu.com', NULL);
 
--- Thêm nhân viên + tài khoản
-INSERT INTO VaiTro (TenVaiTro) VALUES ('Admin'), ('Thủ thư');
-INSERT INTO Quyen (TenQuyen) VALUES ('QuanLySach'), ('QuanLyDocGia');
+-- Dumping data for table library.nxb: ~5 rows (approximately)
+INSERT INTO `nxb` (`IdNXB`, `TenNXB`, `DiaChi`, `SDT`) VALUES
+	(1, 'NXB Trẻ', 'TP. Hồ Chí Minh', '02838293829'),
+	(2, 'NXB Kim Đồng', 'Hà Nội', '02439432024'),
+	(3, 'NXB Giáo Dục', 'Hà Nội', '02437473292'),
+	(4, 'NXB Văn Học', 'TP. Hồ Chí Minh', '02838526482'),
+	(5, 'NXB Hội Nhà Văn', 'Hà Nội', '02438432349');
 
-INSERT INTO VaiTro_Quyen (IdVaiTro, IdQuyen) VALUES
-(1,1),(1,2),(2,1);
+-- Dumping data for table library.phieumuon: ~3 rows (approximately)
+INSERT INTO `phieumuon` (`IdPhieuMuon`, `IdNhanVien`, `IdTheThanhVien`, `NgayMuon`, `NgayHenTra`, `TrangThai`) VALUES
+	(1, 1, 1, '2025-10-07 00:00:00.000000', '2025-10-07 00:00:00.000000', 'DaTra'),
+	(2, 1, 2, '2025-11-24 15:58:26.019000', '2025-11-08 15:58:26.020439', 'DaTra'),
+	(3, 1, 2, '2025-11-24 16:01:27.407950', '2025-12-08 16:01:27.407980', 'DaTra');
 
-INSERT INTO TaiKhoan (IdVaiTro, TenTaiKhoan, MatKhau) VALUES
-(1, 'admin', '123456'),
-(2, 'thuthu', '123456');
+-- Dumping data for table library.phieunhap: ~12 rows (approximately)
+INSERT INTO `phieunhap` (`IdPhieuNhap`, `IdNCC`, `IdNhanVien`, `NgayNhap`, `SoLuongSach`, `TongTienNhap`, `LoaiPhieuNhap`, `TrangThai`) VALUES
+	(1, 1, 1, '2025-11-22 23:30:12.146745', 10, 140000.00, 'Mua', 0),
+	(2, 2, 1, '2025-11-22 23:30:31.120917', 20, 400000.00, 'Mua', 0),
+	(3, 4, 1, '2025-11-22 23:43:28.569482', 4, 56000.00, 'Mua', 1),
+	(4, 1, 1, '2025-11-23 06:02:08.873300', 6, 182000.00, 'Mua', 0),
+	(5, 1, 1, '2025-11-23 06:09:47.802836', 1, 14000.00, 'Mua', 0),
+	(6, 2, 1, '2025-11-23 06:09:57.328740', 5, 340000.00, 'Mua', 0),
+	(7, 1, 1, '2025-11-23 06:11:12.348152', 6, 600000.00, 'Mua', 0),
+	(8, 1, 1, '2025-11-23 06:11:22.640682', 12, 720000.00, 'Mua', 0),
+	(9, 1, 1, '2025-11-23 06:11:33.217925', 15, 762000.00, 'Mua', 0),
+	(10, 3, 1, '2025-11-24 14:14:33.045459', 10, 140000.00, 'Mua', 0),
+	(11, 2, 1, '2025-12-02 12:01:38.881708', 12, 168000.00, 'Mua', 0),
+	(12, 2, 1, '2025-12-02 12:44:53.668747', 12, 168000.00, 'Mua', 0);
 
-INSERT INTO NhanVien (IdTaiKhoan, TenNhanVien, NgaySinh, DiaChi, SDT, Email) VALUES
-(1, 'Nguyễn Văn C', '1990-03-03', 'Hà Nội', '0900000003', 'c@gmail.com');
+-- Dumping data for table library.phieuphat: ~2 rows (approximately)
+INSERT INTO `phieuphat` (`IdPhieuPhat`, `NgayLap`, `TienPhatPhaiNop`, `LyDoPhat`, `TrangThai`) VALUES
+	(1, '2025-11-24 13:59:47.018779', 240000.00, 'Phạt khi trả sách', 'ChuaThu'),
+	(2, '2025-11-24 16:06:39.542991', 240000.00, 'Phạt khi trả sách', 'ChuaThu');
+
+-- Dumping data for table library.quyen: ~72 rows (approximately)
+INSERT INTO `quyen` (`IdQuyen`, `TenQuyen`, `MaQuyen`) VALUES
+	(1, 'Sách', 'SACH_ADD'),
+	(2, 'Sách', 'SACH_EDIT'),
+	(3, 'Sách', 'SACH_DELETE'),
+	(4, 'Sách', 'SACH_VIEW'),
+	(5, 'Sách', 'SACH_EXPORT'),
+	(6, 'Bản sao sách', 'SACH_BANSAO_EDIT'),
+	(7, 'Bản sao sách', 'SACH_BANSAO_DELETE'),
+	(8, 'Bản sao sách', 'SACH_BANSAO_EXPORT'),
+	(9, 'Tác giả', 'TACGIA_ADD'),
+	(10, 'Tác giả', 'TACGIA_EDIT'),
+	(11, 'Tác giả', 'TACGIA_DELETE'),
+	(12, 'Tác giả', 'TACGIA_VIEW'),
+	(13, 'Tác giả', 'TACGIA_EXPORT'),
+	(14, 'Nhà xuất bản', 'NHAXUATBAN_ADD'),
+	(15, 'Nhà xuất bản', 'NHAXUATBAN_EDIT'),
+	(16, 'Nhà xuất bản', 'NHAXUATBAN_DELETE'),
+	(17, 'Nhà xuất bản', 'NHAXUATBAN_VIEW'),
+	(18, 'Nhà xuất bản', 'NHAXUATBAN_EXPORT'),
+	(19, 'Thể loại', 'THELOAI_ADD'),
+	(20, 'Thể loại', 'THELOAI_EDIT'),
+	(21, 'Thể loại', 'THELOAI_DELETE'),
+	(22, 'Thể loại', 'THELOAI_EXPORT'),
+	(23, 'Khách hàng', 'KHACHHANG_ADD'),
+	(24, 'Khách hàng', 'KHACHHANG_EDIT'),
+	(25, 'Khách hàng', 'KHACHHANG_DELETE'),
+	(26, 'Khách hàng', 'KHACHHANG_VIEW'),
+	(27, 'Khách hàng', 'KHACHHANG_EXPORT'),
+	(28, 'Thẻ thành viên', 'THE_THANHVIEN_EDIT'),
+	(29, 'Thẻ thành viên', 'THE_THANHVIEN_DELETE'),
+	(30, 'Thẻ thành viên', 'THE_THANHVIEN_VIEW'),
+	(31, 'Mượn trả sách', 'MUONTRA_ADD'),
+	(32, 'Mượn trả sách', 'MUONTRA_CANCEL'),
+	(33, 'Mượn trả sách', 'MUONTRA_CANCEL_LIST'),
+	(34, 'Mượn trả sách', 'MUONTRA_VIEW'),
+	(35, 'Mượn trả sách', 'MUONTRA_EXPORT'),
+	(36, 'Phạt', 'PHAT_COLLECTED_LIST'),
+	(37, 'Phạt', 'PHAT_CANCEL_LIST'),
+	(38, 'Phạt', 'PHAT_CANCEL'),
+	(39, 'Phạt', 'PHAT_VIEW'),
+	(40, 'Phạt', 'PHAT_EXPORT'),
+	(41, 'Mức phạt', 'MUC_PHAT_ADD'),
+	(42, 'Mức phạt', 'MUC_PHAT_EDIT'),
+	(43, 'Mức phạt', 'MUC_PHAT_DELETE'),
+	(44, 'Mức phạt', 'MUC_PHAT_VIEW'),
+	(45, 'Mức phạt', 'MUC_PHAT_EXPORT'),
+	(46, 'Nhập sách', 'NHAPSACH_ADD'),
+	(47, 'Nhập sách', 'NHAPSACH_CANCEL'),
+	(48, 'Nhập sách', 'NHAPSACH_CANCEL_LIST'),
+	(49, 'Nhập sách', 'NHAPSACH_VIEW'),
+	(50, 'Nhập sách', 'NHAPSACH_EXPORT'),
+	(51, 'Nhà cung cấp', 'NHACUNGCAP_ADD'),
+	(52, 'Nhà cung cấp', 'NHACUNGCAP_EDIT'),
+	(53, 'Nhà cung cấp', 'NHACUNGCAP_DELETE'),
+	(54, 'Nhà cung cấp', 'NHACUNGCAP_VIEW'),
+	(55, 'Nhà cung cấp', 'NHACUNGCAP_EXPORT'),
+	(56, 'Nhân viên', 'NHANVIEN_ADD'),
+	(57, 'Nhân viên', 'NHANVIEN_EDIT'),
+	(58, 'Nhân viên', 'NHANVIEN_DELETE'),
+	(59, 'Nhân viên', 'NHANVIEN_VIEW'),
+	(60, 'Nhân viên', 'NHANVIEN_EXPORT'),
+	(61, 'Tài khoản', 'TAIKHOAN_EDIT'),
+	(62, 'Tài khoản', 'TAIKHOAN_DELETE'),
+	(63, 'Tài khoản', 'TAIKHOAN_VIEW'),
+	(64, 'Vai trò', 'VAITRO_ADD'),
+	(65, 'Vai trò', 'VAITRO_EDIT'),
+	(66, 'Vai trò', 'VAITRO_DELETE'),
+	(67, 'Vai trò', 'VAITRO_VIEW'),
+	(68, 'Thống kê', 'THONGKE_VIEW'),
+	(69, 'Thống kê', 'THONGKE_EXPORT'),
+	(70, 'Thẻ thành viên', 'THETHANHVIEN_EDIT'),
+	(71, 'Thẻ thành viên', 'THETHANHVIEN_DELETE'),
+	(72, 'Thẻ thành viên', 'THETHANHVIEN_VIEW');
+
+-- Dumping data for table library.sach: ~6 rows (approximately)
+INSERT INTO `sach` (`IdSach`, `IdNXB`, `NamXuatBan`, `TenSach`, `MoTa`, `SoTrang`, `SoLuongBanSao`, `GiaTien`) VALUES
+	(1, 1, 2009, 'Kính vạn hoage', 'Truyện dài nhiều tập hehe', 211, 60, 14000.000000000000000000000000000000),
+	(2, 2, 1997, 'Harry Potter and the Philosopher\'s Stone', 'Tập 1 Harry Potter', 350, 46, 20000.000000000000000000000000000000),
+	(3, 1, 2005, 'Cách sống', 'Sách về cách sống', 100, 22, 100000.000000000000000000000000000000),
+	(7, 1, 2010, 'Kính vạn lý trưởng thành', 'Truyện dài nhiều tập', 200, 0, 14000.000000000000000000000000000000),
+	(8, 2, 1997, 'Harry Potter và hòn đá bay', 'Tập 1 Harry Potter', 350, 0, 20000.000000000000000000000000000000),
+	(9, 1, 2005, 'Cách sống của kaiwu', 'Sách về cách sống', 100, 0, 100000.000000000000000000000000000000);
+
+-- Dumping data for table library.sach_tacgia: ~7 rows (approximately)
+INSERT INTO `sach_tacgia` (`IdSach`, `IdTacGia`) VALUES
+	(1, 1),
+	(3, 1),
+	(7, 1),
+	(9, 1),
+	(1, 2),
+	(2, 2),
+	(8, 2);
+
+-- Dumping data for table library.sach_theloai: ~9 rows (approximately)
+INSERT INTO `sach_theloai` (`IdSach`, `IdTheLoai`) VALUES
+	(1, 1),
+	(3, 1),
+	(7, 1),
+	(9, 1),
+	(1, 2),
+	(2, 2),
+	(8, 2),
+	(3, 3),
+	(9, 3);
+
+-- Dumping data for table library.tacgia: ~6 rows (approximately)
+INSERT INTO `tacgia` (`IdTacGia`, `TenTacGia`, `NgaySinh`, `NoiSinh`, `SDT`) VALUES
+	(1, 'Nguyễn Nhật Ánh', NULL, NULL, NULL),
+	(2, 'J.K. Rowling', NULL, NULL, NULL),
+	(3, 'Haruki Murakami', NULL, NULL, NULL),
+	(4, 'George Orwell', NULL, NULL, NULL),
+	(5, 'Ernest Hemingway', NULL, NULL, NULL),
+	(7, 'gà', '2025-10-05 00:00:00.000000', 'Đồng Nai', '0395455082');
+
+-- Dumping data for table library.taikhoan: ~4 rows (approximately)
+INSERT INTO `taikhoan` (`IdTaiKhoan`, `IdVaiTro`, `TenTaiKhoan`, `MatKhau`) VALUES
+	(1, 1, 'admin', '123456'),
+	(2, 2, 'thuthu', '123456'),
+	(10, 9, 'phongba', '123456'),
+	(11, 2, 'dpluu', '123456');
+
+-- Dumping data for table library.theloai: ~0 rows (approximately)
+INSERT INTO `theloai` (`IdTheLoai`, `TenTheloai`) VALUES
+	(1, 'Tiểu thuyết'),
+	(2, 'Khoa học viễn tưởng'),
+	(3, 'Tâm lý học'),
+	(4, 'Văn học thiếu nhi'),
+	(5, 'Kinh tế');
+
+-- Dumping data for table library.thethanhvien: ~0 rows (approximately)
+INSERT INTO `thethanhvien` (`IdTheThanhVien`, `IdDocGia`, `NgayCap`, `NgayHetHan`, `TrangThai`) VALUES
+	(1, 1, '2025-01-01 00:00:00.000000', '2026-01-01 00:00:00.000000', 'HoatDong'),
+	(2, 2, '2025-01-01 00:00:00.000000', '2026-01-01 00:00:00.000000', 'HoatDong');
+
+-- Dumping data for table library.vaitro: ~0 rows (approximately)
+INSERT INTO `vaitro` (`IdVaiTro`, `TenVaiTro`) VALUES
+	(1, 'Quản trị viên'),
+	(2, 'Thủ thư'),
+	(3, 'Nhân viên'),
+	(4, 'Quản lý'),
+	(9, 'Nhập kho'),
+	(10, 'Siêu Admin');
+
+-- Dumping data for table library.vaitro_quyen: ~0 rows (approximately)
+INSERT INTO `vaitro_quyen` (`IdVaiTro`, `IdQuyen`) VALUES
+	(1, 1),
+	(1, 2),
+	(1, 3),
+	(1, 4),
+	(1, 5),
+	(1, 6),
+	(1, 7),
+	(1, 8),
+	(1, 9),
+	(1, 10),
+	(1, 11),
+	(1, 12),
+	(1, 13),
+	(1, 14),
+	(9, 14),
+	(1, 15),
+	(9, 15),
+	(1, 16),
+	(1, 17),
+	(9, 17),
+	(1, 18),
+	(1, 19),
+	(1, 20),
+	(1, 21),
+	(1, 22),
+	(1, 23),
+	(1, 24),
+	(1, 25),
+	(1, 26),
+	(1, 27),
+	(1, 28),
+	(1, 29),
+	(1, 30),
+	(1, 31),
+	(1, 32),
+	(1, 33),
+	(1, 34),
+	(1, 35),
+	(1, 36),
+	(1, 37),
+	(1, 38),
+	(1, 39),
+	(1, 40),
+	(1, 41),
+	(1, 42),
+	(1, 43),
+	(1, 44),
+	(1, 45),
+	(1, 46),
+	(1, 47),
+	(1, 48),
+	(1, 49),
+	(1, 50),
+	(1, 51),
+	(9, 51),
+	(1, 52),
+	(9, 52),
+	(1, 53),
+	(9, 53),
+	(1, 54),
+	(9, 54),
+	(1, 55),
+	(1, 56),
+	(9, 56),
+	(1, 57),
+	(9, 57),
+	(1, 58),
+	(9, 58),
+	(1, 59),
+	(9, 59),
+	(1, 60),
+	(1, 61),
+	(1, 62),
+	(1, 63),
+	(1, 64),
+	(1, 65),
+	(1, 66),
+	(1, 67),
+	(1, 68),
+	(1, 69),
+	(1, 70),
+	(1, 71),
+	(1, 72);
+
+-- Dumping data for table library.__efmigrationshistory: ~0 rows (approximately)
+INSERT INTO `__efmigrationshistory` (`MigrationId`, `ProductVersion`) VALUES
+	('20251016160345_InitDB', '9.0.9'),
+	('20251020061814_PhatEnum', '9.0.10'),
+	('20251112033748_newMigration', '9.0.9');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
