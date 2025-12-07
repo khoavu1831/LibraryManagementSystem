3ec09b06ae1429a8f56eaacb4e5099b95eb1ceee
Nhu Luk <nhubkr1010@gmail.com>
2025-12-07 20:25:38 +0700

    cập nhật lại thống kê sách

--- LMS.Repositories/Repository/ThongKeSachRepository.cs
+++ LMS.Repositories/Repository/ThongKeSachRepository.cs
@@ -13,6 +13,7 @@ public class ThongKeSachRepository
     {
         _db = new LibraryDbContext();
     }
+
     // 1) LẤY TOÀN BỘ SÁCH
     public List<Sach> GetAllSach()
     {
@@ -20,91 +21,138 @@ public class ThongKeSachRepository
                   .Include(s => s.TheLoais)
                   .ToList();
     }
-    // 2) SÁCH ĐANG MƯỢN
+
+    // 2) SÁCH ĐANG MƯỢN 
     public List<(Sach sach, DateTime ngay)> GetSachDangMuon(DateTime ngayFrom, DateTime ngayTo)
     {
-        var sachQuery = _db.Sachs.Include(s => s.TheLoais);
+        var fromDate = ngayFrom.Date;
+        var toDate = ngayTo.Date;
 
-        return (
+        var query =
             from ct in _db.ChiTietPhieuMuons
-            join pm in _db.PhieuMuons
-                on ct.IdPhieuMuon equals pm.IdPhieuMuon
-            join bss in _db.BanSaoSachs
-                on ct.IdBanSaoSach equals bss.IdBanSaoSach
-            join s in sachQuery
-                on bss.IdSach equals s.IdSach
-            where
-                ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.ChuaTra  // đang mượn
-                && pm.NgayMuon >= ngayFrom
-                && pm.NgayMuon <= ngayTo
-            select new { sach = s, ngay = pm.NgayMuon }
-        )
-        .AsEnumerable()
-        .Select(x => (x.sach, x.ngay))
-        .ToList();
+            join pm in _db.PhieuMuons on ct.IdPhieuMuon equals pm.IdPhieuMuon
+            join bss in _db.BanSaoSachs on ct.IdBanSaoSach equals bss.IdBanSaoSach
+            join s in _db.Sachs.Include(x => x.TheLoais) on bss.IdSach equals s.IdSach
+            where ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.ChuaTra
+               && pm.NgayMuon.Date >= fromDate
+               && pm.NgayMuon.Date <= toDate
+            select new
+            {
+                sach = s,
+                ngay = pm.NgayMuon.Date
+            };
+
+        return query
+            .AsEnumerable()
+            .Select(x => (x.sach, x.ngay))
+            .ToList();
     }
 
-    // 3) SÁCH MẤT / HƯ HỎNG
-    public List<(Sach sach, DateTime? ngay)> GetSachMatHuHong(DateTime ngayFrom, DateTime ngayTo)
+    // 3) SÁCH MẤT / HƯ HỎNG (lọc theo ct.NgayTra.Date)
+    //    Mở rộng: nếu có ChiTietPhieuPhat liên quan thì cũng tính (dù ct.TinhTrangTra có đổi)
+    public List<(Sach sach, DateTime? ngayTra)> GetSachMatHuHong(DateTime ngayFrom, DateTime ngayTo)
     {
-        var sachQuery = _db.Sachs.Include(s => s.TheLoais);
+        var fromDate = ngayFrom.Date;
+        var toDate = ngayTo.Date;
 
-        return (
+        var query =
             from ct in _db.ChiTietPhieuMuons
-            join pm in _db.PhieuMuons
-                on ct.IdPhieuMuon equals pm.IdPhieuMuon
-            join bss in _db.BanSaoSachs
-                on ct.IdBanSaoSach equals bss.IdBanSaoSach
-            join s in sachQuery
-                on bss.IdSach equals s.IdSach
+            join pm in _db.PhieuMuons on ct.IdPhieuMuon equals pm.IdPhieuMuon
+            join bss in _db.BanSaoSachs on ct.IdBanSaoSach equals bss.IdBanSaoSach
+            join s in _db.Sachs.Include(x => x.TheLoais) on bss.IdSach equals s.IdSach
             where
-                (ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.Mat
-                || ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.HuHong)
-                && ct.NgayTra >= ngayFrom
-                && ct.NgayTra <= ngayTo
-            select new { sach = s, ngay = ct.NgayTra }
-        )
-        .AsEnumerable()
-        .Select(x => (x.sach, x.ngay))
-        .ToList();
+                ct.NgayTra.HasValue
+                && ct.NgayTra.Value.Date >= fromDate
+                && ct.NgayTra.Value.Date <= toDate
+                && (
+                    ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.Mat
+                    || ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.HuHong
+                    // hoặc có phạt liên quan (đảm bảo: nếu trước kia có phạt mà hiện trạng thay đổi)
+                    || _db.ChiTietPhieuPhats.Any(p => p.IdChiTietPhieuMuon == ct.IdChiTietPhieuMuon)
+                )
+            select new
+            {
+                sach = s,
+                ngay = ct.NgayTra
+            };
+
+        return query
+            .AsEnumerable()
+            .Select(x => (x.sach, x.ngay))
+            .ToList();
     }
 
+    // 4) SÁCH CHƯA MƯỢN 
 
-    // 4) SÁCH CHƯA MƯỢN
-    public List<Sach> GetSachChuaMuon()
+    public int SoBanSaoDangMuonTheoSach(int idSach, DateTime ngayFrom, DateTime ngayTo)
     {
-        var sachDaMuonIds =
-            _db.BanSaoSachs
-               .Where(bss => _db.ChiTietPhieuMuons
-                   .Any(ct => ct.IdBanSaoSach == bss.IdBanSaoSach))
-               .Select(bss => bss.IdSach)
-               .Distinct()
-               .ToList();
-
-        return _db.Sachs
-                  .Include(s => s.TheLoais)
-                  .Where(s => !sachDaMuonIds.Contains(s.IdSach))
-                  .ToList();
+        return (from ct in _db.ChiTietPhieuMuons
+                join pm in _db.PhieuMuons on ct.IdPhieuMuon equals pm.IdPhieuMuon
+                join bss in _db.BanSaoSachs on ct.IdBanSaoSach equals bss.IdBanSaoSach
+                where bss.IdSach == idSach
+                      && ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.ChuaTra
+                      && pm.NgayMuon >= ngayFrom
+                      && pm.NgayMuon <= ngayTo
+                select ct).Count();
     }
+
+
+
     // 5) TỔNG SỐ LƯỢNG SÁCH
     public int TongSoSach()
     {
         return _db.Sachs.Sum(s => s.SoLuongBanSao);
     }
-    // 6) SỐ SÁCH ĐANG MƯỢN
+
+    // 6) SỐ SÁCH ĐANG MƯỢN 
     public int SoSachDangMuon()
     {
         return _db.ChiTietPhieuMuons
-                  .Count(ct => ct.TinhTrangTra ==
-                               ChiTietPhieuMuon.TinhTrangTraEnum.ChuaTra);
+                  .Count(ct => ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.ChuaTra);
     }
-    // 7) SỐ SÁCH MẤT HOẶC HƯ HỎNG
+
+    //  SỐ SÁCH ĐANG MƯỢN theo khoảng ngày (dùng cho chart)
+    public int SoSachDangMuon(DateTime ngayFrom, DateTime ngayTo)
+    {
+        var fromDate = ngayFrom.Date;
+        var toDate = ngayTo.Date;
+
+        return _db.ChiTietPhieuMuons
+                  .Join(_db.PhieuMuons, ct => ct.IdPhieuMuon, pm => pm.IdPhieuMuon, (ct, pm) => new { ct, pm })
+                  .Count(x => x.ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.ChuaTra
+                           && x.pm.NgayMuon.Date >= fromDate
+                           && x.pm.NgayMuon.Date <= toDate);
+    }
+
+    // 7) SỐ SÁCH MẤT HOẶC HƯ HỎNG (toàn bộ)
     public int SoSachMatHuHong()
     {
         return _db.ChiTietPhieuMuons
-                  .Count(ct => ct.TinhTrangTra ==
-                               ChiTietPhieuMuon.TinhTrangTraEnum.Mat
-                            || ct.TinhTrangTra ==
-                               ChiTietPhieuMuon.TinhTrangTraEnum.HuHong);
+                  .Count(ct =>
+                        ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.Mat ||
+                        ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.HuHong
+                       || _db.ChiTietPhieuPhats.Any(p => p.IdChiTietPhieuMuon == ct.IdChiTietPhieuMuon)
+                       );
+    }
+
+    // 7 SỐ SÁCH MẤT HOẶC HƯ HỎNG theo khoảng ngày (dùng cho chart)
+    public int SoSachMatHuHong(DateTime ngayFrom, DateTime ngayTo)
+    {
+        var fromDate = ngayFrom.Date;
+        var toDate = ngayTo.Date;
+
+        var q = _db.ChiTietPhieuMuons
+            .Where(ct => ct.NgayTra.HasValue
+                      && ct.NgayTra.Value.Date >= fromDate
+                      && ct.NgayTra.Value.Date <= toDate)
+            .ToList();
+
+        int count = q.Count(ct =>
+            ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.Mat
+            || ct.TinhTrangTra == ChiTietPhieuMuon.TinhTrangTraEnum.HuHong
+            || _db.ChiTietPhieuPhats.Any(p => p.IdChiTietPhieuMuon == ct.IdChiTietPhieuMuon)
+        );
+
+        return count;
     }
 }
